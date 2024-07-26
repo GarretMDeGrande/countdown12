02fa2b37f6e29a7eeee8dee17713a51d085f562e
GarretMDeGrande <144293313+GarretMDeGrande@users.noreply.github.com>
2024-07-26 15:03:13 -0700

    Add files via upload

---
+++ js/circles.js
@@ -0,0 +1,17 @@
+
+      $("#count-down").TimeCircles(
+       {   
+           circle_bg_color: "#8a7f71",
+           use_background: true,
+           bg_width: 1.0,
+           fg_width: 0.02,
+           time: {
+                Days: { color: "#00A5ED" },
+                Hours: { color: "#00A5ED" },
+                Minutes: { color: "#00A5ED" },
+                Seconds: { color: "#00A5ED" }
+            }
+       }
+    );
+
+    
